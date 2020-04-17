bbbe0706ca9eafdde36ea1542dcbac442f259ec5
Thomas Motz <thomasmotz@gmx.net>
2020-04-17 23:26:39 +0200

    push pull mechanism tested on albeniz.

--- code/pull_code_from_server.py
+++ code/pull_code_from_server.py
@@ -1,10 +1,10 @@
 import os
 
 if __name__ == "__main__":
-    
+
     zip_file_name = "code_brownian_int.zip"
     string_copy = "scp -r " + "motthoma@161.116.80.211://home/motthoma/" + zip_file_name + ' ./'
     os.system(string_copy)
-    
+
     string_unzip = "unzip " + zip_file_name + " -d ./code_unzip"
     os.system(string_unzip)
--- code/push_code_to_server.py
+++ code/push_code_to_server.py
@@ -1,8 +1,12 @@
 import os
+from datetime import datetime
 
 if __name__ == "__main__":
-    
-    zip_file_name = "zip code_brownian_int.zip"
+
+    now = datetime.now()
+    str_now = now.strftime("%d_%m_%Y_%H_%M_%S")
+
+    zip_file_name = "zip code_brownian_int_{}.zip".format(str_now)
     string_zip = zip_file_name + " *.c *.h *.py makefile"
     os.system(string_zip)
 

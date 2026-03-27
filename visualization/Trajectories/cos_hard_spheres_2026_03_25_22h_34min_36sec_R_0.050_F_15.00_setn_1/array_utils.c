26ac8fd930c638becc6d1f8f593617274584b0f0
legmot0 <thomas.motz@liebherr.com>
2026-03-27 22:33:16 +0100

    examples from visualizations added.

---
+++ visualization/Trajectories/cos_hard_spheres_2026_03_25_22h_34min_36sec_R_0.050_F_15.00_setn_1/array_utils.c
@@ -0,0 +1,74 @@
+#include <stdlib.h>
+#include "array_utils.h"
+#include "code_handling.h"
+
+/**
+ * Function that allocates memory for a 2 dimensional array of doubles
+ */
+double **UTILS_calloc_2Ddouble_array(int m, int n)
+{
+    double **array = malloc(m * sizeof(double *));
+    if (!array) return NULL;
+
+    double *data = calloc(m * n, sizeof(double));
+    if (!data) {
+        free(array);
+        return NULL;
+    }
+
+    for (int i = 0; i < m; i++)
+        array[i] = data + i * n;
+
+    return array;
+}
+
+/**
+ * Function that allocates memory for a 2 dimensional array of long ints
+ */
+long int **UTILS_calloc_2Dlint_array(int m, int n) {
+    long int **array = malloc(m * sizeof(long int*));
+    if (!array) return NULL;
+
+    long int *data = calloc(m * n, sizeof(long int));
+    if (!data) { free(array); return NULL; }
+
+    for (int i = 0; i < m; i++) array[i] = data + i * n;
+
+    return array;
+}
+
+/** 
+ * free 2D double data block stored in contiguous array 
+ */
+void UTILS_free_2Ddouble_array(double **array)
+{
+    if (!array) return;
+    /* free data block */
+    free(array[0]);
+    /* free pointer array */
+    free(array);
+}
+
+/** 
+ * free 2D long int data block stored in contiguous array 
+ */
+void UTILS_free_2Dlint_array(long int **array)
+{
+    if (!array) return;
+    /* free data block */
+    free(array[0]);
+    /* free pointer array */
+    free(array);
+}
+
+/**
+ * returns max of doubles a and b 
+ */
+double UTILS_max_double(double a, double b){
+    return a > b ? a : b;
+}
+
+void UTILS_copy_code() {
+    CODEHAND_copy_file_to_dest("array_utils.c");
+    CODEHAND_copy_file_to_dest("array_utils.h");
+}
